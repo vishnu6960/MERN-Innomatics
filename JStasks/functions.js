e5ba0030dfc8fe2cd1a0f63f956a0d3e9376de4f
vishnu <kannasri113@gmail.com>
2024-07-17 23:11:16 +0530

    functions task

--- JStasks/functions.js
+++ JStasks/functions.js
@@ -1,12 +1,12 @@
 //1. Functions without parameters
 function greet(){
-    console.log("Hello, World!")
+    console.log("Hello, World!")        //Hello, World!
 }
 greet()
 
 //2. Functions with parameters
 function addNumbers(a, b){
-    console.log(a+b)
+    console.log(a+b)        //30
 }
 addNumbers(10, 20)
 
@@ -15,17 +15,17 @@ var product = multiply(25, 4)
 function multiply(x, y){
     return x * y
 }
-console.log(product)
+console.log(product)        //100
 
 //4.Combining Functions
 function calculateRectangleArea(l, b){
     return multiply(l, b)
 }
-console.log(calculateRectangleArea(30, 3))
+console.log(calculateRectangleArea(30, 3))      //90
 
 //Default Parameters
 function greetUser(name = "Guest"){
-    console.log(`Hello, ${name}!`)
+    console.log(`Hello, ${name}!`)      //Hello, Ahmed bro!  &&  Hello, Guest
 }
 greetUser("Ahmed bro")
 greetUser()
