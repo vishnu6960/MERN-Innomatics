27ba79f61ba3d776b9f5151add0688bd51902d20
vishnu <kannasri113@gmail.com>
2024-07-31 17:14:09 +0530

    promises

--- JStasks/promises.js
+++ JStasks/promises.js
@@ -1,5 +1,6 @@
 //1. Creating Promises
 function asyncTask(name, delay, shouldReject){
+    console.log(delay)
     return new Promise((res, rej)=>{
         if(shouldReject){
                 rej(`${name} failed`)
@@ -11,29 +12,30 @@ function asyncTask(name, delay, shouldReject){
         }
     })
 }
-asyncTask("vishnu", 1000, false)
+let delay = Math.floor(Math.random() * 2000) + 1000
+asyncTask("vishnu", delay, false)
     .then(data=>console.log(data))
     .catch(err=>console.log(err))
-asyncTask("vishnu", 1000, true)
+asyncTask("vishnu", delay, true)
     .then(data=>console.log(data))
     .catch(err=>console.log(err))
 
 //2.Using Promises
-asyncTask("Task1", 2000, false)
+asyncTask("Task1", delay, false)
     .then(data=>console.log(data))
     .catch(err=>console.log(err))
-asyncTask("Task2", 1000, false)
+asyncTask("Task2", delay, false)
     .then(data=>console.log(data))
     .catch(err=>console.log(err))
-asyncTask("Task3", 3000, true)
+asyncTask("Task3", delay, true)
     .then(data=>console.log(data))
     .catch(err=>console.log(err))
 
 
 //3. Promise.all
-let promise1 = asyncTask("Task1", 2000, false)
-let promise2 = asyncTask("Task2", 2000, false)
-let promise3 = asyncTask("Task3", 1000, true)
+let promise1 = asyncTask("Task1", delay, false)
+let promise2 = asyncTask("Task2", delay, false)
+let promise3 = asyncTask("Task3", delay, true)
 Promise.all([promise1, promise2, promise3]).then(data=>console.log(data)).catch(err=>console.log(err))
 
 //4.Promise.allSettled
