6850fa9b63ca8f4bc9d294240406f7f28b8719fe
vishnu <kannasri113@gmail.com>
2024-08-14 16:43:41 +0530

    todo list

---
+++ JSClasses/toDoList/script.js
@@ -0,0 +1,69 @@
+let cardsContainer = document.getElementById("cardsContainer")
+let todos = []
+document.getElementById("addTask").addEventListener("click", (e)=>{
+    e.preventDefault()
+    let title = document.getElementById("title").value
+    let description = document.getElementById("description").value
+    let category = document.getElementById("category").value
+    let priority = document.getElementById("taskPriority").value
+    
+    let object = {
+        title : title,
+        descr : description,
+        category : category, 
+        priority : priority
+    }
+    addItems(object)
+    todos.push(object)
+    localStorage.setItem('todos', JSON.stringify(todos))
+
+    document.getElementById("formData").reset()
+    
+})
+
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    if(localStorage.getItem("todos")){
+        todos = JSON.parse(localStorage.getItem('todos'))
+        console.log(todos)
+        todos.forEach(item =>{
+            addItems(item)
+        })
+    }
+})
+
+let addItems = (dataObject)=>{
+    let card = document.createElement("div")
+    card.className = 'cardEle'
+    card.innerHTML = `
+        <p><strong>Title :</strong> ${dataObject.title}</p>
+        <p><strong>Description :</strong> ${dataObject.descr}</p>
+        <p><strong>Category :</strong> ${dataObject.category}</p>
+        <p><strong>Priority :</strong> ${dataObject.priority}</p>
+        <button class="deleteButton">Delete</button>
+    `
+    cardsContainer.appendChild(card)
+
+    let cards = document.querySelectorAll(".cardEle")
+    // console.log(cards)
+    cards.forEach(item => {
+        let del = item.querySelector('.deleteButton')
+        del.addEventListener('click', ()=>{
+            // localStorage.removeItem(todos[0])
+            console.log(todos[0])
+            item.remove()
+            // let index = todos.indexOf(dataObject)
+            // if(index != -1){
+            //     todos.splice(index, 1)
+            // }
+            // localStorage.setItem('todos', JSON.stringify(todos))
+        })
+        // console.log(todos)
+    })
+}
+// localStorage.clear() 
+
+
+
+
