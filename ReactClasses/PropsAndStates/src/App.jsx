69b41a6aeb2b281d8cd28f7c9711cf74c90379b1
vishnu <kannasri113@gmail.com>
2024-09-08 23:00:02 +0530

    react classes

--- ReactClasses/PropsAndStates/src/App.jsx
+++ ReactClasses/PropsAndStates/src/App.jsx
@@ -7,10 +7,10 @@ function App() {
   return (
     <div>
 
-      {/* <Cards img="https://tse2.mm.bing.net/th?id=OIP.oiMnEVLWAnSFy9BEen4eJgHaHa&pid=Api&P=0&h=180" title="Tower Burger"
+      <Cards img="https://tse2.mm.bing.net/th?id=OIP.oiMnEVLWAnSFy9BEen4eJgHaHa&pid=Api&P=0&h=180" title="Tower Burger"
       price={399}/>
 
-      <Cards img="https://tse1.mm.bing.net/th?id=OIP.HxFCp9x4AZvkXtUR0aRInwHaE8&pid=Api&P=0&h=180" title="Sandwich"
+      {/* <Cards img="https://tse1.mm.bing.net/th?id=OIP.HxFCp9x4AZvkXtUR0aRInwHaE8&pid=Api&P=0&h=180" title="Sandwich"
       price={299}/> */}
 
       <Counter />
